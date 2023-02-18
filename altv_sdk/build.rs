b57d8c8700f42f801f4040e337c7d69648bd6b28
xshady <fastik036@gmail.com>
2023-02-18 16:51:18 +0300

    implement auto code generation of rust enums from sdk

--- altv_sdk/build.rs
+++ altv_sdk/build.rs
@@ -1,5 +1,8 @@
+use std::fs;
+
 const CPP_SDK_VERSION_DIR: &str = "include/cpp-sdk/version";
-// const CPP_SDK_EVENTS_DIR: &str = "include/cpp-sdk/events";
+const BASE_OBJECT_TYPE_ENUM_FILE: &str = "include/cpp-sdk/objects/IBaseObject.h";
+const EVENT_TYPE_ENUM_FILE: &str = "include/cpp-sdk/events/CEvent.h";
 
 fn main() {
     generate_cpp_sdk_version();
@@ -25,17 +28,21 @@ fn generate_cpp_sdk_version() {
         .write_to_file("src/cpp_sdk_version.rs")
         .expect("Couldn't write bindings!");
 
-    // TODO: copy CEvent::Type enum from CEvent.h automatically
-    // let test_events_enums = bindgen::Builder::default()
-    //     .header(format!("{CPP_SDK_EVENTS_DIR}/CEvent.h"))
-    //     .clang_arg("-std=c++20")
-    //     .clang_arg("-xc++")
-    //     .generate()
-    //     .expect("Unable to generate bindings for version.h");
+    generate_rust_enum_from_cpp(
+        "BaseObjectType",
+        "u8",
+        BASE_OBJECT_TYPE_ENUM_FILE,
+        "enum class Type : uint8_t",
+        "src/base_object_type.rs",
+    );
 
-    // test_events_enums
-    //     .write_to_file("src/test_events_enums.rs")
-    //     .expect("Couldn't write test_events_enums bindings!");
+    generate_rust_enum_from_cpp(
+        "EventType",
+        "u16",
+        EVENT_TYPE_ENUM_FILE,
+        "enum class Type : uint16_t",
+        "src/event_type.rs",
+    );
 }
 
 fn build_rust() {
@@ -45,4 +52,63 @@ fn build_rust() {
         .flag("-DALT_SERVER_API")
         .flag("/std:c++20")
         .compile("alt_sdk");
+}
+
+fn generate_rust_enum_from_cpp(
+    enum_name: &str,
+    enum_type: &str,
+    path: &str,
+    str_to_find: &str,
+    write_to: &str,
+) {
+    let content = String::from_utf8(fs::read(path).unwrap()).unwrap();
+    let str = str_to_find;
+    let idx = content.find(str).unwrap();
+    dbg!(idx);
+
+    let mut open_brace = false;
+    let mut start_idx = 0;
+    let mut end_idx = 0;
+    let chars = content.get(idx..).unwrap().as_bytes();
+
+    for (idx, char) in chars.iter().enumerate() {
+        if open_brace {
+            // println!("");
+            if *char == b'}' {
+                end_idx = idx - 1;
+                break;
+            }
+
+            continue;
+        }
+
+        if *char == b'{' {
+            open_brace = true;
+            start_idx = idx + 1;
+            println!("open brace found at {idx:?}");
+        }
+    }
+
+    if !open_brace {
+        panic!("cannot find open brace of {str:?}")
+    }
+
+    let result_string = String::from_utf8_lossy(&chars[start_idx..=end_idx])
+        .to_string()
+        .split('\n')
+        .filter_map(|val| val.get(2..))
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    fs::write(
+        write_to,
+        format!(
+            "#![allow(non_camel_case_types)]\n\
+            use primitive_enum::primitive_enum;\n\
+            primitive_enum! {{ {enum_name} {enum_type} ;\n\
+                {result_string},\n\
+            }}",
+        ),
+    )
+    .unwrap();
 }
--- altv_sdk/src/base_object_type.rs
+++ altv_sdk/src/base_object_type.rs
@@ -1,21 +1,19 @@
 #![allow(non_camel_case_types)]
-
 use primitive_enum::primitive_enum;
-
-// TODO: copy IBaseObject::Type enum from IBaseObject.h automatically
 primitive_enum! { BaseObjectType u8 ;
-    PLAYER,
-    VEHICLE,
-    BLIP,
-    WEBVIEW,
-    VOICE_CHANNEL,
-    COLSHAPE,
-    CHECKPOINT,
-    WEBSOCKET_CLIENT,
-    HTTP_CLIENT,
-    AUDIO,
-    RML_ELEMENT,
-    RML_DOCUMENT,
-    LOCAL_PLAYER,
-    OBJECT,
+	PLAYER,
+	VEHICLE,
+	BLIP,
+	WEBVIEW,
+	VOICE_CHANNEL,
+	COLSHAPE,
+	CHECKPOINT,
+	WEBSOCKET_CLIENT,
+	HTTP_CLIENT,
+	AUDIO,
+	RML_ELEMENT,
+	RML_DOCUMENT,
+	LOCAL_PLAYER,
+	OBJECT
+,
 }
