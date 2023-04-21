5aba71e1152b9c670e972a5203954f01daa1a4f3
xshady <fastik036@gmail.com>
2023-04-21 23:28:40 +0300

    make ptr property of vehicle model info private

--- example_resource/src/lib.rs
+++ example_resource/src/lib.rs
@@ -4,9 +4,5 @@ pub use altv::prelude::*;
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "full");
 
-    let config = altv::ServerConfig::get();
-    dbg!(config);
-
-    let res = altv::Resource::current();
-    dbg!(res.config.get_custom_key("type"));
+    dbg!(altv::VehicleModelInfo::get_by_hash("sultan2"));
 }
