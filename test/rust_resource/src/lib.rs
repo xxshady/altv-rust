8a2b97290c97145d3ec7d591da2c9481d8101cb4
xshady <fastik036@gmail.com>
2023-06-11 18:55:28 +0300

    remove player tests

--- test/rust_resource/src/lib.rs
+++ test/rust_resource/src/lib.rs
@@ -3,12 +3,10 @@ mod base_object_pool_funcs;
 mod base_object_funcs;
 mod helpers;
 mod weapon_model_info;
-mod player;
 
 use base_object_funcs::test_base_object_funcs;
 use base_object_pool_funcs::test_base_object_pool_funcs;
 use mvalue::test_mvalue;
-use player::test_player;
 use weapon_model_info::test_weapon_model_info;
 
 #[altv::main]
@@ -23,8 +21,6 @@ fn main() -> impl altv::IntoVoidResult {
     test_mvalue();
     altv::log!("#################### weapon_model_info");
     test_weapon_model_info();
-    altv::log!("#################### player");
-    test_player();
 
     altv::stop_server();
 }
