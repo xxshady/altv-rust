603e69e72162f57e2a8e4d091328ff9ea6ec90a1
xshady <fastik036@gmail.com>
2023-07-11 01:57:57 +0300

    update altv_sdk

--- altv_sdk/src/alt_classes/IPlayer.h
+++ altv_sdk/src/alt_classes/IPlayer.h
@@ -139,52 +139,5 @@ f32 GetNonSpatialVolume(const alt::IPlayer* ptr) {
 void SetNonSpatialVolume(alt::IPlayer* ptr, f32 volume) {
     return ptr->SetNonSpatialVolume(volume);
 }
-void SetAmmoSpecialType(alt::IPlayer* ptr, u32 ammoHash, AmmoSpecialType_t ammoSpecialType) {
-    return ptr->SetAmmoSpecialType(ammoHash, static_cast<alt::AmmoSpecialType>(ammoSpecialType));
-}
-AmmoSpecialType_t GetAmmoSpecialType(const alt::IPlayer* ptr, u32 ammoHash) {
-    return static_cast<uint32_t>(ptr->GetAmmoSpecialType(ammoHash));
-}
-void SetAmmoFlags(alt::IPlayer* ptr, u32 ammoHash, bool ammoFlags_infiniteAmmo, bool ammoFlags_addSmokeOnExplosion, bool ammoFlags_fuse, bool ammoFlags_fixedAfterExplosion) {
-    return ptr->SetAmmoFlags(ammoHash, create_ammo_flags_from_params(
-        ammoFlags_infiniteAmmo,
-        ammoFlags_addSmokeOnExplosion,
-        ammoFlags_fuse,
-        ammoFlags_fixedAfterExplosion
-        ));
-}
-alt::AmmoFlags GetAmmoFlags(const alt::IPlayer* ptr, u32 ammoHash) {
-    return ptr->GetAmmoFlags(ammoHash);
-}
-void SetAmmoMax(alt::IPlayer* ptr, u32 ammoHash, i32 ammoMax) {
-    return ptr->SetAmmoMax(ammoHash, ammoMax);
-}
-i32 GetAmmoMax(const alt::IPlayer* ptr, u32 ammoHash) {
-    return ptr->GetAmmoMax(ammoHash);
-}
-void SetAmmoMax50(alt::IPlayer* ptr, u32 ammoHash, i32 ammoMax50) {
-    return ptr->SetAmmoMax50(ammoHash, ammoMax50);
-}
-i32 GetAmmoMax50(const alt::IPlayer* ptr, u32 ammoHash) {
-    return ptr->GetAmmoMax50(ammoHash);
-}
-void SetAmmoMax100(alt::IPlayer* ptr, u32 ammoHash, i32 ammoMax100) {
-    return ptr->SetAmmoMax100(ammoHash, ammoMax100);
-}
-i32 GetAmmoMax100(const alt::IPlayer* ptr, u32 ammoHash) {
-    return ptr->GetAmmoMax100(ammoHash);
-}
-void AddDecoration(alt::IPlayer* ptr, u32 collection, u32 overlay) {
-    return ptr->AddDecoration(collection, overlay);
-}
-void RemoveDecoration(alt::IPlayer* ptr, u32 collection, u32 overlay) {
-    return ptr->RemoveDecoration(collection, overlay);
-}
-void ClearDecorations(alt::IPlayer* ptr) {
-    return ptr->ClearDecorations();
-}
-std::vector<alt::CDecoration> GetDecorations(const alt::IPlayer* ptr) {
-    return ptr->GetDecorations();
-}
 
 } // namespace
